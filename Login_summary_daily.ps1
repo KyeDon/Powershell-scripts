40c35e41ca2e2c0640dc0bbd3250890e240afaec
KyeDon <47357003+KyeDon@users.noreply.github.com>
2020-05-05 19:19:47 +0100

    Update Login_summary_daily.ps1

--- Login_summary_daily.ps1
+++ Login_summary_daily.ps1
@@ -17,31 +17,21 @@ $SMTPServer = "smtp.office365.com" #May need to change this
 $SMTPPort = 587 #May need to change this
 
 #Changing these variables is optional
-[datetime]$date = (get-date).AddDays(-1) #Change -1 for a different interval.
+[datetime]$Date = (get-date).AddDays(-1) #Change -1 for a different interval.
 $hostname = [System.Net.Dns]::GetHostByName($env:computerName).HostName
 
-#Enable auditing on login in local computer policy
+#Enable auditing on login in local computer policy (assumes no AD)
 $CurrentAudit = (auditpol /get /subcategory:"Logon")[4]
 if( -not $CurrentAudit.Contains("Failure") -and -not $CurrentAudit.contains("Success")) {
     auditpol /set /subcategory:"Logon" /failure:enable
     auditpol /set /subcategory:"Logon" /success:enable
 }
 
-#Get event viewer events 4624=success 4625=failure
-$Success = get-eventlog -logname security -instanceid 4624 -after $date
-$Failure = get-eventlog -logname security -instanceid 4625 -after $date
-$S_count = ($Success | measure).count
+##Failures
+#Get event viewer events 4625=failure
+$Failure = get-eventlog -logname security -instanceid 4625 -after $Date
 $F_count = ($failure | measure).count
-$S_minus = $S_count -1 #Used for accessing array later
 $F_minus = $F_count -1 #Used for accessing array later
-
-#Success loop
-$i = 0
-while ($i -lt $S_count)
-{
-    [array]$S_table += $Success[$i] | Format-Table -AutoSize | Out-String
-    $i += 1
-}
 
 #Failure loop
 $i = 0
@@ -50,6 +40,19 @@ while ($i -lt $F_count)
     [array]$F_table += $Failure[$i] | Format-Table -AutoSize | Out-String
     $i += 1
 }
+
+##Success
+#Get event viewer events with XML query
+$Query = @"
+<QueryList>
+  <Query Id="0" Path="Security">
+    <Select Path="Security">*[System[(EventID=4624)] and EventData[Data[@Name='TargetDomainName']!='Font Driver Host'] and EventData[Data[@Name='TargetDomainName']!='Window Manager'] and EventData[Data[@Name='TargetDomainName']!='NT AUTHORITY']]</Select>
+  </Query>
+</QueryList>
+"@
+
+$Filtered = Get-WinEvent -FilterXml $Query
+$Success = $filtered | Where-Object { $_.timecreated -gt $Date }
 
 ##Send out email report
 $Subject = "Daily audit report on $hostname"
@@ -86,3 +89,18 @@ $SMTPClient = New-Object Net.Mail.SmtpClient($SMTPServer, $SMTPPort)
 $SMTPClient.EnableSsl = $true
 $SMTPClient.Credentials = New-Object System.Net.NetworkCredential($EmailFrom, $EmailPW);
 $SMTPClient.Send($SMTPMessage)
+
+
+
+
+$Success = get-eventlog -logname security -instanceid 4624 -after $date
+$S_count = ($Success | measure).count
+$S_minus = $S_count -1 #Used for accessing array later
+
+#Success loop
+$i = 0
+while ($i -lt $S_count)
+{
+    [array]$S_table += $Success[$i] | Format-Table -AutoSize | Out-String
+    $i += 1
+}
