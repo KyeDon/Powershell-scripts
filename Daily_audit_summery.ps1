40e34defc9ab1d66f11244b30302a129c8d220e9
KyeDon <47357003+KyeDon@users.noreply.github.com>
2020-05-06 20:44:37 +0100

    Update Daily_audit_summery.ps1

--- Daily_audit_summery.ps1
+++ Daily_audit_summery.ps1
@@ -1,4 +1,24 @@
-[datetime]$Date = (get-date).AddDays(-1)
+<#
+Audit successful/failed logins in last day and send out a small breakdown email.
+This script will enable auditing for login success/failures if they are disabled.
+This means that the first time it runs it may not find anything even if
+there were logins previously.
+This script is intended to be paired with a task scheduler task to run
+inline with the $date variable. Full documentation can be found on my Github.
+Written by Kye Donaldson for Cyberdan Ltd 26/04/2020
+Github - https://github.com/KyeDon
+#>
+
+##Main variables - These are the only variables that may need changing
+$EmailTo = "Example@Example.co.uk" #Change this
+$EmailFrom = "smtpconnect@Example.co.uk" #Change this
+$EmailPW = "password1" #Change this
+$SMTPServer = "smtp.office365.com" #May need to change this
+$SMTPPort = 587 #May need to change this
+
+#Changing these variables is optional
+[datetime]$Date = (get-date).AddDays(-1) #Change -1 for a different interval.
+$hostname = [System.Net.Dns]::GetHostByName($env:computerName).HostName
 
 $Query = @"
 <QueryList>
@@ -13,10 +33,13 @@ $Success = $filtered | Where-Object { $_.timecreated -gt $Date }
 $S_count = ($Success | measure).count
 $S_minus = $S_count -1 #Used for accessing array later
 
-foreach ( $S_obj in $success)
+$i = 0
+foreach ($S_obj in $success)
 {
     if ($i -le $S_minus) {
         [array]$username += $success[$i].properties[5].value
         $i += 1
     }
 }
+
+$grouped = $username | Group-Object -NoElement
